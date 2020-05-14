49c794fce8cf79d637b2263b0ef9a1b56d3f04c3
Robert Rand <rxtreme@gmail.com>
2020-05-14 14:24:29 -0400

    added new gates, typing rules, deriviations

--- Typing.v
+++ Typing.v
@@ -1,4 +1,4 @@
-Require Import Types.
+Require Export Types.
 
 (* Programs *)
 
@@ -10,7 +10,18 @@ Inductive prog :=
 | CNOT (n1 n2 : nat)
 | seq (p1 p2 : prog).
 
-Infix ";" := seq (at level 60).
+Infix ";" := seq (at level 51, right associativity).
+
+(* I, X, Y and Z as derived gates *)
+Definition Z' n : prog := S n ; S n.
+Definition X' n : prog := H n ; Z' n; H n.
+Definition Y' n : prog := S n; X' n; Z' n; S n.
+Definition I' n : prog := H n; H n.
+
+(* Other common gates *)
+Definition CZ m n : prog := H n ; CNOT m n ; H n.
+Definition SWAP m n : prog := CNOT m n; CNOT n m; CNOT m n.
+
 
 (** Basic Typing judgements *)
 
@@ -89,3 +100,87 @@ Proof.
     + eapply cap_elim_r. apply H0.
 Qed.
 
+(** Tensor rules *)
+
+Notation s := Datatypes.S.
+
+Parameter Singleton : GType -> Prop.
+Axiom SI: Singleton I.
+Axiom SX : Singleton X. 
+Axiom SZ : Singleton Z. 
+Axiom S_neg : forall A, Singleton A -> Singleton (neg A).
+Axiom S_i : forall A, Singleton A -> Singleton (i A).
+Axiom S_mul : forall A B, Singleton A -> Singleton B -> Singleton (A * B).
+
+Axiom tensor_inc : forall g n E A A',
+    Singleton A ->
+    g n :: (A → A') ->
+    g (s n) ::  E ⊗ A → E ⊗ A'. 
+
+Axiom tensor_inc2 : forall (g : nat -> nat -> prog) m n E A A' B B',
+    Singleton A ->
+    Singleton B ->
+    g m n :: (A ⊗ B → A' ⊗ B') ->
+    g (s m) (s n) ::  E ⊗ A ⊗ B → E ⊗ A' ⊗ B'. 
+
+Axiom tensor_inc2_r : forall (g : nat -> nat -> prog) m n E A A' B B',
+    Singleton A ->
+    Singleton B ->
+    g m n :: (A ⊗ B → A' ⊗ B') ->
+    g m (s n) ::  A ⊗ E ⊗ B → A' ⊗ E ⊗ B'. 
+
+(* For flipping CNOTs. Could have CNOT specific rule. *)
+Axiom tensor2_comm : forall (g : nat -> nat -> prog) m n  A A' B B',
+    Singleton A ->
+    Singleton B ->
+    g m n :: A ⊗ B → A' ⊗ B' ->
+    g n m :: B ⊗ A → B' ⊗ A'.
+
+(** Arrow rules *)
+
+Axiom arrow_mul : forall p A A' B B',
+    p :: A → A' ->
+    p :: B → B' ->
+    p :: A * B → A' * B'.
+
+Axiom arrow_i : forall p A A',
+    p :: A → A' ->
+    p :: i A → i A'.
+
+Axiom arrow_neg : forall p A A',
+    p :: A → A' ->
+    p :: -A → -A'.  
+  
+Axiom arrow_comp : forall p1 p2 A A' A'',
+    p1 :: A → A' ->
+    p2 :: A' → A'' ->
+    p1 ; p2 :: A → A''.
+
+Axiom seq_assoc : forall p1 p2 p3 A,
+    p1 ; (p2 ; p3) :: A <-> (p1 ; p2) ; p3 :: A.
+
+Hint Resolve HTypes STypes CNOTTypes : base_types_db.
+Hint Resolve cap_elim_l cap_elim_r : base_types_db.
+
+Hint Resolve HTypes STypes CNOTTypes : typing_db.
+Hint Resolve cap_intro cap_elim_l cap_elim_r : typing_db.
+Hint Resolve arrow_comp : typing_db.
+
+Lemma ZTypes : Z' 0 :: (X → -X) ∩ (Z → Z).
+Proof.
+  unfold Z'.
+  repeat apply cap_intro.  
+  - repeat eapply arrow_comp.
+    + eauto with base_types_db.
+    + eauto with base_types_db.
+  apply cap_intro.  
+  eapply arrow_comp.
+  
+  
+Lemma XTypes : X' 0 :: (X → X) ∩ (Z → -Z).
+Proof.
+  unfold X'.
+  
+  specialize HTypes as H1.
+  apply cap_intro.
+  - 
