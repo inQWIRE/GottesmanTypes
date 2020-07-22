a52f81feb312429b6f4de45f98b0849b48a89117
Robert Rand <rnrand@gmail.com>
2020-07-22 17:04:56 -0400

    proofs of GHZ

--- examples/GHZ.v
+++ examples/GHZ.v
@@ -85,3 +85,44 @@ Proof. type_check_base. Qed.
 
 (* In all cases results should have one qubit unentangled *)
 
+(** ** GHZ with Separability Judgements *)
+
+Lemma SEP0_ZZZ : SEP0 :: Z × Z × Z → X × Z × Z.
+Proof.
+  rewrite sep_expansion3 at 1; auto with sep_db.
+  eapply eq_arrow_r.
+  apply cap_arrow_distributes; apply cap_intro.
+  apply cap_arrow_distributes; apply cap_intro.
+  type_check_base.
+  type_check_base.
+  type_check_base.
+  normalize_mul.
+  rewrite (all_I_sep_l X _); auto with sep_db.
+  rewrite sep_cap_I_l; auto with sep_db.
+  rewrite sep_cap_I_l; auto with sep_db.
+  rewrite (cap_I_l_gen (Z ⊗ I)); auto with sep_db.
+  rewrite (all_I_sep_l Z I); auto with sep_db.
+  rewrite sep_cap_same_l; auto with sep_db.
+  rewrite cap_I_l; auto with sing_db.
+Qed.
+
+(** Unentangling one qubit *)
+
+Lemma PSEP10_ZZZ : PSEP10 :: Z × Z × Z → Z × (X ⊗ X ∩ Z ⊗ Z).
+Proof.
+  rewrite sep_expansion3 at 1; auto with sep_db.
+  eapply eq_arrow_r.
+  apply cap_arrow_distributes; apply cap_intro.
+  apply cap_arrow_distributes; apply cap_intro.
+  type_check_base.
+  type_check_base.
+  type_check_base.
+  normalize_mul.
+  rewrite (cap_comm (I ⊗ _)).
+  rewrite (all_I_sep_l Z (I ⊗ I)); auto with sep_db.
+  rewrite sep_cap_I_l; auto with sep_db.
+  rewrite sep_cap_I_l; auto with sep_db.
+  rewrite (cap_I_l_gen (X ⊗ X)); auto with sep_db.
+Qed.
+
+
