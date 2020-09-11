5b5bbf813e0175b3fa66978a2ec1c2a17f0d6cc7
Robert Rand <rnrand@gmail.com>
2020-09-11 18:15:16 -0500

    expanded derived gates with Y and pair types

--- DerivedGates.v
+++ DerivedGates.v
@@ -30,3 +30,57 @@ Lemma SWAPTypes : (SWAP 0 1) :: (X ⊗ I → I ⊗ X) ∩ (I ⊗ X → X ⊗ I) 
                                (Z ⊗ I → I ⊗ Z) ∩ (I ⊗ Z → Z ⊗ I).
 Proof. type_check_base. Qed.  
 
+(* Types on Y *)
+
+Lemma HTypesY : H' 0 :: Y → - Y.
+Proof. type_check_base. Qed.
+
+Lemma STypesY : S' 0 :: Y → - X.
+Proof. type_check_base. Qed.
+
+Lemma ITypesY : I' 0 :: Y → Y.
+Proof. type_check_base. Qed.
+
+Lemma XTypesY : X' 0 :: Y → - Y.
+Proof. type_check_base. Qed.
+
+Lemma ZTypesY : Z' 0 :: Y → - Y.
+Proof. type_check_base. Qed.
+
+Lemma YTypesY : Y' 0 :: Y → Y.
+Proof. type_check_base. Qed.
+
+Lemma CNOTTypesY : CNOT 0 1 :: (Y ⊗ I → Y ⊗ X) ∩ (I ⊗ Y → Z ⊗ Y).
+Proof. type_check_base. Qed.
+
+Lemma CZTypesY : CZ 0 1 :: (Y ⊗ I → Y ⊗ Z) ∩ (I ⊗ Y → Z ⊗ Y).
+Proof. type_check_base. Qed.
+
+(* All types for CNOT and CZ *)
+
+Lemma CNOTTypesPairs : CNOT 0 1 :: (X ⊗ X →  X ⊗ I) ∩
+                                  (X ⊗ Y →  Y ⊗ Z) ∩
+                                  (X ⊗ Z → -Y ⊗ Y) ∩
+                                  (Y ⊗ X →  Y ⊗ I) ∩
+                                  (Y ⊗ Y → -X ⊗ Z) ∩
+                                  (Y ⊗ Z →  X ⊗ Y) ∩
+                                  (Z ⊗ X →  Z ⊗ X) ∩
+                                  (Z ⊗ Y →  I ⊗ Y) ∩
+                                  (Z ⊗ Z →  I ⊗ Z).
+Proof. type_check_base. Qed.
+
+Lemma CZTypesPairs : CZ 0 1 :: (X ⊗ X →  Y ⊗ Y) ∩
+                              (X ⊗ Y → -Y ⊗ X) ∩
+                              (X ⊗ Z →  X ⊗ I) ∩
+                              (Y ⊗ X → -X ⊗ Y) ∩
+                              (Y ⊗ Y →  X ⊗ X) ∩
+                              (Y ⊗ Z →  Y ⊗ I) ∩
+                              (Z ⊗ X →  I ⊗ X) ∩
+                              (Z ⊗ Y →  I ⊗ Y) ∩
+                              (Z ⊗ Z →  Z ⊗ Z).
+Proof. type_check_base. Qed.
+
+  
+
+
+
